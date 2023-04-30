c9b6120c67814e9d0b3079388213696996ce4612
haaju28 <95388693+haaju28@users.noreply.github.com>
2023-04-30 19:15:49 +0700

    fix

--- src/main/java/com/finalProject/foodStore/repositories/CategoryRepository.java
+++ src/main/java/com/finalProject/foodStore/repositories/CategoryRepository.java
@@ -1,9 +1,14 @@
 package com.finalProject.foodStore.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 import com.finalProject.foodStore.models.Category;
-
+import com.finalProject.foodStore.dto.CategoryDto;
 public interface CategoryRepository extends JpaRepository<Category, Integer>{
+    @Query("select p from category p ")
+    List<Category> getAllCategories();
 
+    @Query("select new com.finalProject.foodStore.dto.CategoryDto(c.id, c.name) from category c inner  join food p on p.category.id = c.id group by c.id")
+    List<CategoryDto> getCategoryAndProduct();
 }
